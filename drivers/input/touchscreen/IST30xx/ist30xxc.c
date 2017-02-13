086a079290b0daee96161899d8434f9fb10368af
Zhao Wei Liew <zhaoweiliew@gmail.com>
2017-02-13 18:42:01 +0800

    input: touchscreen: ist30xx: Handle more framebuffer hints
    Resume touchscreen functions during doze mode to allow the user
    to interact with the screen.
    
    Change-Id: Ic55a80b99bafe0e661b5933838929d76f414a837
    Signed-off-by: Darshan Manjrekar <darshanmanjrekar1205@gmail.com>

--- drivers/input/touchscreen/IST30xx/ist30xxc.c
+++ drivers/input/touchscreen/IST30xx/ist30xxc.c
@@ -1351,7 +1351,9 @@ static int fb_notifier_callback(struct notifier_block *self,
 	if (evdata && evdata->data && event == FB_EVENT_BLANK &&
 	    ist_data && ist_data->client) {
 		blank = evdata->data;
-		if (*blank == FB_BLANK_UNBLANK)
+		if (*blank == FB_BLANK_UNBLANK
+				|| *blank == FB_BLANK_NORMAL
+				|| *blank == FB_BLANK_VSYNC_SUSPEND)
 			schedule_work(&ist_data->fb_notify_work);
 		else if (*blank == FB_BLANK_POWERDOWN) {
 			flush_work(&ist_data->fb_notify_work);
