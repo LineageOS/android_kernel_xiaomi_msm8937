3fe111664cd92fc0dd371182f6b3ce350f59ae51
Kristina Martsenko <kristina.martsenko@arm.com>
2017-05-03 16:37:46 +0100

    arm64: hw_breakpoint: fix watchpoint matching for tagged pointers
    commit 7dcd9dd8cebe9fa626af7e2358d03a37041a70fb upstream.
    
    When we take a watchpoint exception, the address that triggered the
    watchpoint is found in FAR_EL1. We compare it to the address of each
    configured watchpoint to see which one was hit.
    
    The configured watchpoint addresses are untagged, while the address in
    FAR_EL1 will have an address tag if the data access was done using a
    tagged address. The tag needs to be removed to compare the address to
    the watchpoints.
    
    Currently we don't remove it, and as a result can report the wrong
    watchpoint as being hit (specifically, always either the highest TTBR0
    watchpoint or lowest TTBR1 watchpoint). This patch removes the tag.
    
    Fixes: d50240a5f6ce ("arm64: mm: permit use of tagged pointers at EL0")
    Acked-by: Mark Rutland <mark.rutland@arm.com>
    Acked-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Kristina Martsenko <kristina.martsenko@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Git-commit: d8f0905a0d4347061291e2ce3fcd75196df5851a
    Git-repo: https://android.googlesource.com/kernel/common
    Signed-off-by: Shiraz Hashim <shashim@codeaurora.org>

--- arch/arm64/kernel/hw_breakpoint.c
+++ arch/arm64/kernel/hw_breakpoint.c
@@ -37,6 +37,7 @@
 #include <asm/cputype.h>
 #include <asm/sysreg.h>
 #include <asm/system_misc.h>
+#include <asm/uaccess.h>
 
 /* Breakpoint currently in use for each BRP. */
 static DEFINE_PER_CPU(struct perf_event *, bp_on_reg[ARM_MAX_BRP]);
@@ -694,7 +695,7 @@ static int watchpoint_handler(unsigned long addr, unsigned int esr,
 
 		/* Check if the watchpoint value matches. */
 		val = read_wb_reg(AARCH64_DBG_REG_WVR, i);
-		if (val != (addr & ~alignment_mask))
+		if (val != (untagged_addr(addr) & ~alignment_mask))
 			goto unlock;
 
 		/* Possible match, check the byte address select to confirm. */
