909f83adc6c487e95c423aacb5667e52b1023985
Darshan Manjrekar <darshanmanjrekar1205@gmail.com>
2018-05-08 22:16:24 +0530

    drivers: leds: leds-gpio: Import Xiaomi changes

--- drivers/leds/leds-gpio.c
+++ drivers/leds/leds-gpio.c
@@ -4,6 +4,7 @@
  * Copyright (C) 2007 8D Technologies inc.
  * Raphael Assenat <raph@8d.com>
  * Copyright (C) 2008 Freescale Semiconductor, Inc.
+ * Copyright (C) 2016-2018 XiaoMi, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -21,6 +22,31 @@
 #include <linux/platform_device.h>
 #include <linux/slab.h>
 #include <linux/workqueue.h>
+#ifdef CONFIG_MACH_XIAOMI_MSM8937
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/pinctrl/consumer.h>
+#include <linux/delay.h>
+#define DUTY_CLCLE 50
+#define ADJUST_NUM 15
+#define JUSTTIMES 6
+#define JUST_DELAY 9
+
+static s64 dealt;
+static DEFINE_SPINLOCK(infrared_lock);
+struct gpio_ir_tx_packet {
+	struct completion done;
+	unsigned int gpio_nr;
+	bool high_active;
+	u32 pulse;
+	u32 space;
+	unsigned int *buffer;
+	unsigned int length;
+	unsigned int next;
+	bool on;
+	bool abort;
+};
+#endif
 
 struct gpio_led_data {
 	struct led_classdev cdev;
@@ -33,6 +59,10 @@ struct gpio_led_data {
 	int (*platform_gpio_blink_set)(unsigned gpio, int state,
 			unsigned long *delay_on, unsigned long *delay_off);
 };
+
+#ifdef CONFIG_MACH_XIAOMI_MSM8937
+struct mutex ir_lock;
+#endif
 
 static void gpio_led_work(struct work_struct *work)
 {
@@ -55,6 +85,9 @@ static void gpio_led_set(struct led_classdev *led_cdev,
 		container_of(led_cdev, struct gpio_led_data, cdev);
 	int level;
 
+#ifdef CONFIG_MACH_XIAOMI_MSM8937
+        int ret = 0;
+#endif
 	if (value == LED_OFF)
 		level = 0;
 	else
@@ -76,7 +109,15 @@ static void gpio_led_set(struct led_classdev *led_cdev,
 							 NULL, NULL);
 			led_dat->blinking = 0;
 		} else
+#ifdef CONFIG_MACH_XIAOMI_MSM8937
+			ret = gpio_direction_output(led_dat->gpio, level);
+		if (ret) {
+			printk("infrared unable to set dir for gpio [%d]\n",
+			       led_dat->gpio);
+		}
+#else
 			gpio_set_value(led_dat->gpio, level);
+#endif
 	}
 }
 
@@ -90,6 +131,140 @@ static int gpio_blink_set(struct led_classdev *led_cdev,
 	return led_dat->platform_gpio_blink_set(led_dat->gpio, GPIO_LED_BLINK,
 						delay_on, delay_off);
 }
+
+#ifdef CONFIG_MACH_XIAOMI_MSM8937
+static void gpio_ir_tx_set(struct gpio_ir_tx_packet *gpkt, bool on)
+{
+	if (gpkt->high_active)
+		gpio_direction_output(gpkt->gpio_nr, on);
+	else
+		gpio_direction_output(gpkt->gpio_nr, !on);
+}
+
+static s64 time_adjust(struct gpio_ir_tx_packet *gpkt)
+{
+	s64 now;
+	int i;
+	unsigned long flags;
+
+	spin_lock_irqsave(&infrared_lock, flags);
+
+	now = ktime_to_us(ktime_get());
+	for (i = 0; i < ADJUST_NUM; i++) {
+		gpio_ir_tx_set(gpkt, false);
+	}
+	spin_unlock_irqrestore(&infrared_lock, flags);
+
+	return (ktime_to_us(ktime_get()) - now) / ADJUST_NUM;
+}
+
+static long pwm_ir_tx_work(void *arg)
+{
+	struct gpio_ir_tx_packet *gpkt = arg;
+	unsigned long flags;
+
+	/* disable irq for acurracy timing */
+	spin_lock_irqsave(&infrared_lock, flags);
+	for (; gpkt->next < gpkt->length; gpkt->next++) {
+		if (gpkt->next & 0x01) {	/* space */
+			gpio_ir_tx_set(gpkt, false);
+			if (gpkt->buffer[gpkt->next] >= dealt)
+				udelay(gpkt->buffer[gpkt->next] - dealt);
+		} else if (!gpkt->pulse || !gpkt->space) {
+			gpio_ir_tx_set(gpkt, true);
+			if (gpkt->buffer[gpkt->next] >= dealt)
+				udelay(gpkt->buffer[gpkt->next] - dealt);
+		} else {	/* pulse with soft carrier */
+			while (gpkt->buffer[gpkt->next]) {
+				unsigned int usecs;
+
+				usecs = gpkt->on ? gpkt->pulse : gpkt->space;
+				usecs = min(usecs, gpkt->buffer[gpkt->next]);
+
+				gpio_ir_tx_set(gpkt, gpkt->on);
+
+				if (usecs >= dealt)
+					udelay(usecs - dealt);
+
+				gpkt->buffer[gpkt->next] -= usecs;
+				gpkt->on = !gpkt->on;
+			}
+		}
+	}
+
+	gpio_ir_tx_set(gpkt, false);
+	spin_unlock_irqrestore(&infrared_lock, flags);
+	return gpkt->next ? : -ERESTARTSYS;
+}
+
+static int gpio_ir_tx_transmit_with_delay(struct gpio_ir_tx_packet *gpkt)
+{
+
+	int cpu, i, rc = -ENODEV;
+	dealt = time_adjust(gpkt);
+	if (dealt > JUST_DELAY) {
+		for (i = 0; i < JUSTTIMES; i++) {
+			dealt = time_adjust(gpkt);
+		}
+		printk("infrared time_adjust fail!! dealt=%lld\n", dealt);
+		if (dealt > JUST_DELAY)
+			dealt = gpkt->pulse;
+	}
+	for_each_online_cpu(cpu) {
+		if (cpu != 0) {
+			rc = work_on_cpu(cpu, pwm_ir_tx_work, gpkt);
+		}
+	}
+
+	if (rc == -ENODEV) {
+		pr_warn("pwm-ir: can't ron on the auxilliary cpu\n");
+	}
+	printk("real dealt =%lld\n", dealt);
+	return rc;
+}
+
+static ssize_t transmit_show(struct device *dev,
+			     struct device_attribute *attr, char *buf)
+{
+	struct led_classdev *led_cdev = dev_get_drvdata(dev);
+
+	return sprintf(buf, "%d\n", led_cdev->brightness);
+}
+
+static ssize_t transmit_store(struct device *dev,
+			      struct device_attribute *attr,
+			      const char *buf, size_t count)
+{
+	int *temp_buf = (int *)buf;
+	int rc = 0;
+	u32 carrier, period;
+
+	struct gpio_ir_tx_packet gpkt = { };
+	struct led_classdev *led_cdev = dev_get_drvdata(dev);
+	struct gpio_led_data *led_dat =
+	    container_of(led_cdev, struct gpio_led_data, cdev);
+
+	mutex_lock(&ir_lock);
+
+	carrier = temp_buf[0];
+	period = NSEC_PER_MSEC / carrier;
+
+	gpkt.pulse = period * DUTY_CLCLE / 100;
+	gpkt.space = period - gpkt.pulse;
+
+	gpkt.gpio_nr = led_dat->gpio;
+	gpkt.high_active = 1 /*gdata->tx_high_active */ ;
+	gpkt.buffer = (unsigned int *)&temp_buf[1];
+	gpkt.length = ((int)count / 4 - 1);
+
+	rc = gpio_ir_tx_transmit_with_delay(&gpkt);
+	mutex_unlock(&ir_lock);
+
+	return rc;
+}
+
+static DEVICE_ATTR(transmit, 0664, transmit_show, transmit_store);
+#endif
 
 static int create_gpio_led(const struct gpio_led *template,
 	struct gpio_led_data *led_dat, struct device *parent,
@@ -139,6 +314,11 @@ static int create_gpio_led(const struct gpio_led *template,
 	if (ret < 0)
 		return ret;
 
+#ifdef CONFIG_MACH_XIAOMI_MSM8937
+	if (strcmp(led_dat->cdev.name, "infrared") == 0) {
+		device_create_file(led_dat->cdev.dev, &dev_attr_transmit);
+	}
+#endif
 	return 0;
 }
 
@@ -174,7 +354,11 @@ static struct gpio_leds_priv *gpio_leds_create_of(struct platform_device *pdev)
 	if (!count)
 		return ERR_PTR(-ENODEV);
 
+#ifdef CONFIG_MACH_XIAOMI_MSM8937
+	for_each_child_of_node(np, child)
+#else
 	for_each_available_child_of_node(np, child)
+#endif
 		if (of_get_gpio(child, 0) == -EPROBE_DEFER)
 			return ERR_PTR(-EPROBE_DEFER);
 
@@ -183,7 +367,11 @@ static struct gpio_leds_priv *gpio_leds_create_of(struct platform_device *pdev)
 	if (!priv)
 		return ERR_PTR(-ENOMEM);
 
+#ifdef CONFIG_MACH_XIAOMI_MSM8937
+	for_each_child_of_node(np, child) {
+#else
 	for_each_available_child_of_node(np, child) {
+#endif
 		struct gpio_led led = {};
 		enum of_gpio_flags flags;
 		const char *state;
@@ -203,8 +391,14 @@ static struct gpio_leds_priv *gpio_leds_create_of(struct platform_device *pdev)
 				led.default_state = LEDS_GPIO_DEFSTATE_OFF;
 		}
 
+#ifdef CONFIG_MACH_XIAOMI_MSM8937
+		led.retain_state_suspended =
+		    (unsigned)of_property_read_bool(child,
+						    "retain-state-suspended");
+#else
 		if (of_get_property(child, "retain-state-suspended", NULL))
 			led.retain_state_suspended = 1;
+#endif
 
 		ret = create_gpio_led(&led, &priv->leds[priv->num_leds++],
 				      &pdev->dev, NULL);
@@ -227,7 +421,10 @@ static const struct of_device_id of_gpio_leds_match[] = {
 	{},
 };
 
+#ifndef CONFIG_MACH_XIAOMI_MSM8937
 MODULE_DEVICE_TABLE(of, of_gpio_leds_match);
+#endif
+
 #else /* CONFIG_OF_GPIO */
 static struct gpio_leds_priv *gpio_leds_create_of(struct platform_device *pdev)
 {
@@ -237,9 +434,21 @@ static struct gpio_leds_priv *gpio_leds_create_of(struct platform_device *pdev)
 
 static int gpio_led_probe(struct platform_device *pdev)
 {
+#ifdef CONFIG_MACH_XIAOMI_MSM8937
+	struct gpio_led_platform_data *pdata = pdev->dev.platform_data;
+	struct pinctrl *pinctrl;
+#else
 	struct gpio_led_platform_data *pdata = dev_get_platdata(&pdev->dev);
+#endif
 	struct gpio_leds_priv *priv;
 	int i, ret = 0;
+
+#ifdef CONFIG_MACH_XIAOMI_MSM8937
+	pinctrl = devm_pinctrl_get_select_default(&pdev->dev);
+	if (IS_ERR(pinctrl))
+		dev_warn(&pdev->dev,
+			 "pins are not configured from the driver\n");
+#endif
 
 	if (pdata && pdata->num_leds) {
 		priv = devm_kzalloc(&pdev->dev,
@@ -267,6 +476,9 @@ static int gpio_led_probe(struct platform_device *pdev)
 	}
 
 	platform_set_drvdata(pdev, priv);
+#ifdef CONFIG_MACH_XIAOMI_MSM8937
+	mutex_init(&ir_lock);
+#endif
 
 	return 0;
 }
@@ -279,6 +491,9 @@ static int gpio_led_remove(struct platform_device *pdev)
 	for (i = 0; i < priv->num_leds; i++)
 		delete_gpio_led(&priv->leds[i]);
 
+#ifdef CONFIG_MACH_XIAOMI_MSM8937
+	platform_set_drvdata(pdev, NULL);
+#endif
 	return 0;
 }
 
