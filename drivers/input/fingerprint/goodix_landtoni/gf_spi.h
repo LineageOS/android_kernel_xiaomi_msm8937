527c57d5a72f7a47044ebfce6e852f1710f9fc10
Ryan Andri <ryanandri@hotmail.com>
2019-09-07 19:58:25 +0700

    fingerprint: goodix_landtoni: changes for fb notifier
    * Drop useless pr_info
    * simplify register fb notifier
    * unregister fb notifier when module exit

--- drivers/input/fingerprint/goodix_landtoni/gf_spi.h
+++ drivers/input/fingerprint/goodix_landtoni/gf_spi.h
@@ -85,7 +85,7 @@ struct gf_dev {
 #ifdef GF_FASYNC
 	struct fasync_struct *async;
 #endif
-	struct notifier_block notifier;
+	struct notifier_block gf_notifier;
 	char device_available;
 	char fb_black;
 	struct wake_lock ttw_wl;
