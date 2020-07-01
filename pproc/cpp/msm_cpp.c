b001d84f318da252c0bfe549b06b8c59d32828e5
Gurram Pravalika <gpravali@codeaurora.org>
2020-07-01 22:47:21 +0530

    msm: camera: Enable CPP and JPEG module
    1. Add support for new attributes.
    2. remove the redundant internal interface.
    
    [cafebabe: Apply to legacy marshmallow camera stack]
    
    Change-Id: Ia7fc44fa3515d9462dec3e7f6ead782936e17aa9
    Signed-off-by: Gurram Pravalika <gpravali@codeaurora.org>

--- pproc/cpp/msm_cpp.c
+++ pproc/cpp/msm_cpp.c
@@ -267,7 +267,7 @@ static void msm_enqueue(struct msm_device_queue *queue,
 static int msm_cpp_notify_frame_done(struct cpp_device *cpp_dev,
 	uint8_t put_buf);
 static int32_t cpp_load_fw(struct cpp_device *cpp_dev, char *fw_name_bin);
-static void cpp_timer_callback(unsigned long data);
+static void cpp_timer_callback(struct timer_list *cpp_t);
 
 uint8_t legacy_m_induce_error;
 static int msm_cpp_enable_debugfs(struct cpp_device *cpp_dev);
@@ -1608,7 +1608,7 @@ error:
 	return;
 }
 
-void cpp_timer_callback(unsigned long data)
+static void cpp_timer_callback(struct timer_list *cpp_t)
 {
 	struct msm_cpp_work_t *work =
 		legacy_m_cpp_timer.data.cpp_dev->work;
@@ -4067,8 +4067,8 @@ static int cpp_probe(struct platform_device *pdev)
 	atomic_set(&legacy_m_cpp_timer.used, 0);
 	/* install timer for cpp timeout */
 	CPP_DBG("Installing legacy_m_cpp_timer\n");
-	setup_timer(&legacy_m_cpp_timer.legacy_m_cpp_timer,
-		cpp_timer_callback, (unsigned long)&legacy_m_cpp_timer);
+	timer_setup(&legacy_m_cpp_timer.legacy_m_cpp_timer,
+		cpp_timer_callback, 0);
 	cpp_dev->fw_name_bin = NULL;
 	cpp_dev->max_timeout_trial_cnt = MSM_CPP_MAX_TIMEOUT_TRIAL;
 	if (rc == 0)
