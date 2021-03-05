408f58ea6a02af7ef8874d61a8cf742209277de2
Swetha Chikkaboraiah <schikk@codeaurora.org>
2021-03-05 17:53:40 +0530

    drivers: soc: qcom: Fix compilation errors
    Fix compilation errors with resepct to wakeup_source.
    
    Change-Id: Idaf7543a7dd6032ff63a92cdc843a4c300f82693
    Signed-off-by: Swetha Chikkaboraiah <schikk@codeaurora.org>

--- drivers/soc/qcom/bam_dmux.c
+++ drivers/soc/qcom/bam_dmux.c
@@ -266,7 +266,7 @@ static DEFINE_RWLOCK(ul_wakeup_lock);
 static DECLARE_WORK(kickoff_ul_wakeup, kickoff_ul_wakeup_func);
 static int bam_connection_is_active;
 static int wait_for_ack;
-static struct wakeup_source bam_wakelock;
+static struct wakeup_source *bam_wakelock;
 static int a2_pc_disabled;
 static DEFINE_MUTEX(dfab_status_lock);
 static int dfab_is_on;
@@ -2160,7 +2160,7 @@ static void grab_wakelock(void)
 	BAM_DMUX_LOG("%s: ref count = %d\n", __func__,
 						wakelock_reference_count);
 	if (wakelock_reference_count == 0)
-		__pm_stay_awake(&bam_wakelock);
+		__pm_stay_awake(bam_wakelock);
 	++wakelock_reference_count;
 	spin_unlock_irqrestore(&wakelock_reference_lock, flags);
 }
@@ -2180,7 +2180,7 @@ static void release_wakelock(void)
 						wakelock_reference_count);
 	--wakelock_reference_count;
 	if (wakelock_reference_count == 0)
-		__pm_relax(&bam_wakelock);
+		__pm_relax(bam_wakelock);
 	spin_unlock_irqrestore(&wakelock_reference_lock, flags);
 }
 
@@ -2788,7 +2788,7 @@ static int bam_dmux_probe(struct platform_device *pdev)
 	init_completion(&shutdown_completion);
 	complete_all(&shutdown_completion);
 	INIT_DELAYED_WORK(&ul_timeout_work, ul_timeout);
-	wakeup_source_init(&bam_wakelock, "bam_dmux_wakelock");
+	bam_wakelock = wakeup_source_register(NULL, "bam_dmux_wakelock");
 	init_srcu_struct(&bam_dmux_srcu);
 
 	subsys_h = subsys_notif_register_notifier("modem", &restart_notifier);
