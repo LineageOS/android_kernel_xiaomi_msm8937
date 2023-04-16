c320a92d5563569e028c721bac71bb507d4cd3a9
me-cafebabe <me.cafebabe@gmail.com>
2023-04-16 21:50:26 +0800

    mi8937: ts: Initialize driver depending on panel name for ulysse
    References:
    - https://github.com/MiCode/Xiaomi_Kernel_OpenSource/blob/ulysse-n-oss/drivers/input/touchscreen/ft5435/focaltech_core.c#L1016
    - https://github.com/MiCode/Xiaomi_Kernel_OpenSource/blob/ulysse-n-oss/drivers/input/touchscreen/gt9xx/gt9xx.c#L2267
    
    Change-Id: I362d3d3636bacfdb2cf85700cf577f1a7ce91719

--- touchscreen/ts_init.c
+++ touchscreen/ts_init.c
@@ -1,5 +1,13 @@
 #include <linux/module.h>
+#include <linux/printk.h>
+#include <linux/string.h>
+#include <xiaomi-msm8937/mach.h>
 #include <xiaomi-msm8937/touchscreen.h>
+
+#if IS_ENABLED(CONFIG_MACH_FAMILY_XIAOMI_ULYSSE)
+#define MDSS_MAX_PANEL_LEN 256
+extern char mdss_mdp_panel[MDSS_MAX_PANEL_LEN];
+#endif
 
 /*
 #include <xiaomi-msm8937/touchscreen.h>
@@ -27,6 +35,23 @@ extern int xiaomi_msm8937_touchscreen_ist30xx_init(void);
 
 static int __init xiaomi_msm8937_touchscreen_init(void)
 {
+#if IS_ENABLED(CONFIG_MACH_FAMILY_XIAOMI_ULYSSE)
+	if (xiaomi_msm8937_mach_get_family() == XIAOMI_MSM8937_MACH_FAMILY_ULYSSE) {
+		pr_info("%s: Mach family is ULYSSE, Initialize touchscreen driver depending on panel name.\n", __func__);
+		if (strstr(mdss_mdp_panel, "mdss_dsi_sc_ili9881c_720p_video")) {
+			pr_info("%s: Initialize FocalTech Touchscreen driver, for ili9881c panel.\n", __func__);
+#if IS_ENABLED(CONFIG_TOUCHSCREEN_FTS_MI8937)
+			return xiaomi_msm8937_touchscreen_fts_ts_init();
+#endif
+		} else {
+			pr_info("%s: Initialize Goodix Touchscreen driver, for other panels.\n", __func__);
+#if IS_ENABLED(CONFIG_TOUCHSCREEN_GT9XX_MI8937)
+			return xiaomi_msm8937_touchscreen_gtp_init();
+#endif
+		}
+	}
+#endif
+
 #if IS_ENABLED(CONFIG_TOUCHSCREEN_ATMEL308U_MI8937)
 	xiaomi_msm8937_touchscreen_mxt_init();
 #endif
