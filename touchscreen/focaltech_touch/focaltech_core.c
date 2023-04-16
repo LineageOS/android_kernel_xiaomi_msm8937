9c8abca58b43c229c11cf2c357bff95767f07e54
me-cafebabe <me.cafebabe@gmail.com>
2023-04-16 18:44:16 +0800

    mi8937: ts: fts: Stop using initcall
    Change-Id: I4c348bc4621bcef8f5afef3b1490c0c6bbe9c998

--- touchscreen/focaltech_touch/focaltech_core.c
+++ touchscreen/focaltech_touch/focaltech_core.c
@@ -3463,11 +3463,7 @@ static void __exit fts_ts_exit(void)
 	fts_ts_spi_exit();
 }
 
-#ifdef CONFIG_ARCH_QTI_VM
 module_init(fts_ts_init);
-#else
-late_initcall(fts_ts_init);
-#endif
 module_exit(fts_ts_exit);
 
 MODULE_AUTHOR("FocalTech Driver Team");
