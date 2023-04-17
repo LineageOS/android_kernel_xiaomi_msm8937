71ec1e75319b3e079b428312be55ee099363d30e
me-cafebabe <me.cafebabe@gmail.com>
2023-04-17 22:21:15 +0800

    of_batterydata: Add fallback batterydata if no best match is found
    * As per stock behavior
    
    Change-Id: Iaa5cab6878d6fe51de747697b7297e55e4d2e762

--- drivers/of/of_batterydata.c
+++ drivers/of/of_batterydata.c
@@ -13,6 +13,9 @@
 #include <linux/batterydata-lib.h>
 #include <linux/of_batterydata.h>
 #include <linux/power_supply.h>
+#if IS_ENABLED(CONFIG_MACH_XIAOMI_MSM8937)
+#include <xiaomi-msm8937/mach.h>
+#endif
 
 static int of_batterydata_read_lut(const struct device_node *np,
 			int max_cols, int max_rows, int *ncols, int *nrows,
@@ -318,6 +321,9 @@ struct device_node *of_batterydata_get_best_profile(
 	int delta = 0, best_delta = 0, best_id_kohm = 0, id_range_pct,
 		i = 0, rc = 0, limit = 0;
 	bool in_range = false;
+#if IS_ENABLED(CONFIG_MACH_XIAOMI_MSM8937)
+	bool xiaomi_msm8937_no_battery_data = false;
+#endif
 
 	/* read battery id range percentage for best profile */
 	rc = of_property_read_u32(batterydata_container_node,
@@ -369,18 +375,41 @@ struct device_node *of_batterydata_get_best_profile(
 		}
 	}
 
+#if IS_ENABLED(CONFIG_MACH_XIAOMI_MSM8937)
+	if (xiaomi_msm8937_mach_get() && best_node == NULL) {
+		xiaomi_msm8937_no_battery_data = true;
+		pr_warn("No battery data found, Add fallback battery data\n");
+		for_each_child_of_node(batterydata_container_node, node) {
+			best_node = node;
+			best_id_kohm = batt_id_kohm;
+
+			rc = of_property_read_string(node, "qcom,battery-type",
+					&battery_type);
+			if (!rc && strcmp(battery_type, "Generic_Battery") == 0) {
+				pr_warn("Found Generic_Battery, use it as fallback battery data\n");
+				break;
+			}
+		}
+	}
+#endif
+
 	if (best_node == NULL) {
 		pr_err("No battery data found\n");
 		return best_node;
 	}
 
 	/* check that profile id is in range of the measured batt_id */
+#if IS_ENABLED(CONFIG_MACH_XIAOMI_MSM8937)
+	if (xiaomi_msm8937_mach_get() && xiaomi_msm8937_no_battery_data)
+		goto skip_check_id;
+#endif
 	if (abs(best_id_kohm - batt_id_kohm) >
 			((best_id_kohm * id_range_pct) / 100)) {
 		pr_err("out of range: profile id %d batt id %d pct %d\n",
 			best_id_kohm, batt_id_kohm, id_range_pct);
 		return NULL;
 	}
+skip_check_id:
 
 	rc = of_property_read_string(best_node, "qcom,battery-type",
 							&battery_type);
