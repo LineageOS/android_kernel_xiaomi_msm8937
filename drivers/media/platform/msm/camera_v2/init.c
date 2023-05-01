ab70ce7a8d6ee06c4352c7c30d88e22d6f48afed
me-cafebabe <me.cafebabe@gmail.com>
2023-05-01 21:45:37 +0800

    camera_v2: Don't enable if legacy camera stack is built in
    Change-Id: I3acdf0acf74233f8835852539481a28c16625619

--- drivers/media/platform/msm/camera_v2/init.c
+++ drivers/media/platform/msm/camera_v2/init.c
@@ -1,5 +1,8 @@
 #include <linux/module.h>
 #include <linux/printk.h>
+#if IS_ENABLED(CONFIG_MACH_XIAOMI_MSM8937)
+#include <xiaomi-msm8937/mach.h>
+#endif
 
 /*
 extern bool msmb_camera_enable;
@@ -13,6 +16,21 @@ bool msmb_camera_enable = false;
 static int __init msmb_camera_init(void)
 {
 	msmb_camera_enable = true;
+
+#if IS_ENABLED(CONFIG_MSMB_CAMERA_LEGACY)
+#if IS_ENABLED(CONFIG_MACH_XIAOMI_MSM8937)
+	if (xiaomi_msm8937_mach_get())
+		msmb_camera_enable = false;
+#endif
+#endif
+
+#if IS_ENABLED(CONFIG_MSMB_CAMERA_LEGACY_M)
+#if IS_ENABLED(CONFIG_MACH_XIAOMI_LAND) || IS_ENABLED(CONFIG_MACH_XIAOMI_PRADA)
+	if (xiaomi_msm8937_mach_get() == XIAOMI_MSM8937_MACH_LAND ||
+		xiaomi_msm8937_mach_get() == XIAOMI_MSM8937_MACH_PRADA)
+		msmb_camera_enable = false;
+#endif
+#endif
 
 	if (msmb_camera_enable)
 		pr_info("Enable MSMB Camera Stack.\n");
