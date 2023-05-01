ae316a5d00758073098615f3c74989b1ceda9e9b
me-cafebabe <me.cafebabe@gmail.com>
2023-05-01 20:55:01 +0800

    camera_v2-legacy: Don't enable on land and prada
    * If Legacy (Marshmallow) camera stack is enabled

--- init.c
+++ init.c
@@ -1,5 +1,8 @@
 #include <linux/module.h>
 #include <linux/printk.h>
+#if IS_ENABLED(CONFIG_MACH_XIAOMI_MSM8937)
+#include <xiaomi-msm8937/mach.h>
+#endif
 
 /*
 extern bool camera_legacy_enable;
@@ -13,6 +16,14 @@ bool camera_legacy_enable = false;
 static int __init camera_legacy_init(void)
 {
 	camera_legacy_enable = true;
+
+#if IS_ENABLED(CONFIG_MSMB_CAMERA_LEGACY_M)
+#if IS_ENABLED(CONFIG_MACH_XIAOMI_LAND) || IS_ENABLED(CONFIG_MACH_XIAOMI_PRADA)
+	if (xiaomi_msm8937_mach_get() == XIAOMI_MSM8937_MACH_LAND ||
+		xiaomi_msm8937_mach_get() == XIAOMI_MSM8937_MACH_PRADA)
+		camera_legacy_enable = false;
+#endif
+#endif
 
 	if (camera_legacy_enable)
 		pr_info("Enable Legacy Camera Stack.\n");
