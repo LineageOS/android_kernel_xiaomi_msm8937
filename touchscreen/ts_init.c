79f283d9bc832b00a444ac304dc1d28bd60e4c90
me-cafebabe <me.cafebabe@gmail.com>
2023-05-11 19:48:47 +0800

    mi439: ts: Do not init in charger mode

--- touchscreen/ts_init.c
+++ touchscreen/ts_init.c
@@ -1,4 +1,8 @@
+#if IS_ENABLED(CONFIG_PARSE_ANDROIDBOOT_MODE)
+#include <linux/androidboot_mode.h>
+#endif
 #include <linux/export.h>
+#include <linux/module.h>
 #include <linux/types.h>
 #include <xiaomi-sdm439/touchscreen.h>
 
@@ -12,3 +16,16 @@
 */
 enum xiaomi_sdm439_touchscreen_types xiaomi_sdm439_touchscreen_type = XIAOMI_SDM439_TOUCHSCREEN_UNKNOWN;
 EXPORT_SYMBOL(xiaomi_sdm439_touchscreen_type);
+
+static int __init xiaomi_sdm439_touchscreen_init(void)
+{
+	xiaomi_sdm439_touchscreen_type = XIAOMI_SDM439_TOUCHSCREEN_UNKNOWN;
+
+#if IS_ENABLED(CONFIG_PARSE_ANDROIDBOOT_MODE)
+	if (androidboot_mode_get() == ANDROIDBOOT_MODE_CHARGER)
+		xiaomi_sdm439_touchscreen_type = XIAOMI_SDM439_TOUCHSCREEN_DISABLE;
+#endif
+
+	return 0;
+}
+module_init(xiaomi_sdm439_touchscreen_init);
