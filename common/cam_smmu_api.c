dadba5ae7ed8477ed9c8539f029786a2b986a1f2
me-cafebabe <me.cafebabe@gmail.com>
2023-05-27 16:10:43 +0800

    camera_v2-legacy: Cherry-pick DMA/SMMU changes from msm-4.14 commit "Enable camera on trinket"

--- common/cam_smmu_api.c
+++ common/cam_smmu_api.c
@@ -816,13 +816,13 @@ static int cam_smmu_map_buffer_and_add_to_list(int idx, int ion_fd,
 	mapping_info->attach = attach;
 	mapping_info->table = table;
 	mapping_info->paddr = sg_dma_address(table->sgl);
-	mapping_info->len = (size_t)sg_dma_len(table->sgl);
+	mapping_info->len = (size_t)buf->size;
 	mapping_info->dir = dma_dir;
 	mapping_info->ref_count = 1;
 
 	/* return paddr and len to client */
 	*paddr_ptr = sg_dma_address(table->sgl);
-	*len_ptr = (size_t)sg_dma_len(table->sgl);
+	*len_ptr = (size_t)buf->size;
 
 	if (!*paddr_ptr || !*len_ptr) {
 		pr_err("Error: Space Allocation failed!\n");
