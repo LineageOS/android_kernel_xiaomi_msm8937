00416ab41b09bd66a16823c84e6f7640a8272d0c
Yumi Yukimura <me.cafebabe@gmail.com>
2023-10-30 13:32:52 +0800

    Revert "camera_v2: Only initialize in normal boot mode"
    * We may boot into system from charger mode
    
    This reverts commit 1a709e2a08101a3bbe5193e8cdc134ee23d3989d.
    
    Change-Id: Ice63a842b24c94e75466ec5b9c0b46def7d04575

--- drivers/media/platform/msm/camera_v2/init.c
+++ drivers/media/platform/msm/camera_v2/init.c
@@ -1,6 +1,3 @@
-#if IS_ENABLED(CONFIG_PARSE_ANDROIDBOOT_MODE)
-#include <linux/androidboot_mode.h>
-#endif
 #include <linux/module.h>
 #include <linux/printk.h>
 #if IS_ENABLED(CONFIG_MACH_XIAOMI_MSM8937)
@@ -18,11 +15,6 @@ bool msmb_camera_enable = false;
 
 static int __init msmb_camera_init(void)
 {
-#if IS_ENABLED(CONFIG_PARSE_ANDROIDBOOT_MODE)
-	if (androidboot_mode_get() != ANDROIDBOOT_MODE_NORMAL)
-		return -ENODEV;
-#endif
-
 	msmb_camera_enable = true;
 
 #if IS_ENABLED(CONFIG_MSMB_CAMERA_LEGACY)
