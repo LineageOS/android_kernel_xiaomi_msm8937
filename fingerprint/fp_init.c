70d53ccb5b827c328dc9b21bf29df5c250d11d9d
Yumi Yukimura <me.cafebabe@gmail.com>
2023-10-30 13:42:34 +0800

    Revert "mi8937: fp: Don't init when booting into charger mode"
    * We may boot into system from charger mode
    
    This reverts commit 1adbb7ae244d3a2794af2a03fabc94c05c4bad88.
    
    Change-Id: I88e0ebb6da0617254b289f2f0a016a1645672b90

--- fingerprint/fp_init.c
+++ fingerprint/fp_init.c
@@ -1,6 +1,3 @@
-#if IS_ENABLED(CONFIG_PARSE_ANDROIDBOOT_MODE)
-#include <linux/androidboot_mode.h>
-#endif
 #include <linux/export.h>
 #include <linux/init.h>
 #include <linux/module.h>
@@ -47,11 +44,6 @@ static int __init xiaomi_msm8937_fingerprint_init(void)
 {
 	if (!xiaomi_msm8937_mach_get())
 		return -ENODEV;
-
-#if IS_ENABLED(CONFIG_PARSE_ANDROIDBOOT_MODE)
-	if (androidboot_mode_get() == ANDROIDBOOT_MODE_CHARGER)
-		return -ENODEV;
-#endif
 
 #if IS_ENABLED(CONFIG_MACH_XIAOMI_UGG)
 	if (xiaomi_msm8937_mach_get() == XIAOMI_MSM8937_MACH_UGG) {
