0c47253dee838ae4bc1c4be1e6cc0d60fcfce615
Yumi Yukimura <me.cafebabe@gmail.com>
2023-11-22 15:22:00 +0800

    fs: proc: Override android boot device parameter from LK's one
    * The (secondary) bootloader may want to specify a different
      boot device, through `androidboot.bootdevice=` parameter.
    * We now relies on `androidboot.boot_devices=` parameter
      to boot, however the stock bootloader don't handle this.
      Do it here.
    
    Change-Id: Id5c3b7f2433b459ae1cb1b42a6007993de23bb97

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -14,10 +14,24 @@
 #define INITRAMFS_STR_LEN (sizeof(INITRAMFS_STR_FIND) - 1)
 #endif
 
+#ifdef CONFIG_PROC_CMDLINE_OVERRIDE_ANDROID_BOOT_DEVICES
+#define ANDROID_BOOT_DEVICES_STR_FIND "androidboot.boot_devices=soc/"
+#define ANDROID_BOOT_DEVICES_STR_LEN (sizeof(ANDROID_BOOT_DEVICES_STR_FIND) - 1)
+
+#define LK_BOOTDEVICE_VALUE_LEN 13 // 7824900.sdhci
+static char saved_lk_bootdevice[LK_BOOTDEVICE_VALUE_LEN + 1];
+static int __init lk_bootdevice_setup(char *str) {
+	strlcpy(saved_lk_bootdevice, str, sizeof(saved_lk_bootdevice));
+	return 1;
+}
+__setup("androidboot.bootdevice=", lk_bootdevice_setup);
+#endif
+
 static char proc_command_line[COMMAND_LINE_SIZE];
 
 static void proc_command_line_init(void) {
-	char *offset_addr;
+	char *offset_addr, *offset_addr2;
+	size_t value_len;
 #ifdef CONFIG_PROC_CMDLINE_APPEND_ANDROID_FSTAB_SUFFIX
 	const char *android_fstab_suffix;
 #endif
@@ -33,6 +47,20 @@ static void proc_command_line_init(void) {
 #ifdef CONFIG_PROC_CMDLINE_APPEND_ANDROID_FSTAB_SUFFIX
 	if (of_property_read_string(of_find_node_by_path("/"), "android,fstab_suffix", &android_fstab_suffix) == 0)
 		snprintf(proc_command_line, COMMAND_LINE_SIZE, "%s androidboot.fstab_suffix=%s", proc_command_line, android_fstab_suffix);
+#endif
+
+#ifdef CONFIG_PROC_CMDLINE_OVERRIDE_ANDROID_BOOT_DEVICES
+	offset_addr = strstr(proc_command_line, ANDROID_BOOT_DEVICES_STR_FIND);
+	if (offset_addr && saved_lk_bootdevice[0]) {
+		offset_addr += ANDROID_BOOT_DEVICES_STR_LEN;
+		offset_addr2 = strstr(offset_addr, " ");
+		if (!offset_addr2)
+			offset_addr2 = offset_addr + strlen(offset_addr);
+		value_len = offset_addr2 - offset_addr;
+		memset(offset_addr, ' ', value_len);
+		memcpy(offset_addr, &saved_lk_bootdevice,
+			value_len < LK_BOOTDEVICE_VALUE_LEN ? value_len : LK_BOOTDEVICE_VALUE_LEN);
+	}
 #endif
 }
 
